13ebe6d7ea4a8b9dd5495a1aa1179ab00802bacd
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-10-25 14:13:59 +0200

    Creata logica numero divisibile per 3 su jss

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Seleziono container 
+const containerJs = document.getElementById("container");
+
+// Creo la nostra "lista" di numeri da 1 a 100
+for ( i=1; i<=100; i++) {
+    let divJs = document.createElement("div");
+    divJs.classList.add("square");
+    divJs.append(i);
+    containerJs.append(divJs);
+
+    // se il numero è divisibile per 3 aggiungo classe fizz 
+
+    if ( i%3===0 ) {
+        divJs.classList.add("fizz");
+        divJs.innerHTML="fizz";
+    }
+
+    // se il numero è divisibile per 5 aggiungo classe buzz
+
+    // se il numero è divisibile per 3 e 5 aggiungo classe fizzbuzz
+
+}
